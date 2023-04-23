d286e5b44fe4cb3d9bc6a1d41d3a34d5a7d2c8e2
quenio roger <queniorojer_rm@hotmail.com>
2023-04-23 14:26:49 -0300

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Lang;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\App;
 use Session;
 
 class LoginController extends Controller
