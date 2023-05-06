712c51c427a1df36aae58ab3b025bbddeca640f8
quenio roger <queniorojer_rm@hotmail.com>
2023-05-06 17:11:23 -0300

    update

--- database/migrations/2022_10_23_184331_reset_password_log.php
+++ database/migrations/2022_10_23_184331_reset_password_log.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users_reset_password_log', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamps();
         });
     }
