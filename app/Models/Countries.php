68333397a3d83492dcce5b271979016912523e14
quenio roger <queniorojer_rm@hotmail.com>
2023-05-05 23:14:37 -0300

    update

--- app/Models/Countries.php
+++ app/Models/Countries.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\App;
 
 class Countries extends Model
 {
