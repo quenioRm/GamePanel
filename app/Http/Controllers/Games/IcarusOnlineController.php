3de4ba1fd03b8de40eb59266c200ec6453a4fb46
quenio roger <queniorojer_rm@hotmail.com>
2023-05-10 14:22:36 -0300

    update

--- app/Http/Controllers/Games/IcarusOnlineController.php
+++ app/Http/Controllers/Games/IcarusOnlineController.php
@@ -22,8 +22,8 @@ class IcarusOnlineController extends Controller
         if($account->isBlockEmailDomain == 1)
             return response()->xml(null, $status = 200, $headers = [], $xmlRoot = 'root', $encoding = null);
 
-        if($account->permission == 0 && $alert == false)
-            return response()->xml(null, $status = 200, $headers = [], $xmlRoot = 'root', $encoding = null);
+        // if($account->permission == 0 && $alert == false)
+        //     return response()->xml(null, $status = 200, $headers = [], $xmlRoot = 'root', $encoding = null);
 
         $data = array(
             'getParamInfo' => [
