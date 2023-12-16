b27c220d6017348e023796c20330a03725afae60
quenio roger <queniorojer_rm@hotmail.com>
2023-12-16 04:37:17 -0300

    update

--- database/migrations/2023_04_13_211311_alerts.php
+++ database/migrations/2023_04_13_211311_alerts.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('alerts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->string('type');
             $table->datetime('date');
             $table->datetime('showAfterDate');
